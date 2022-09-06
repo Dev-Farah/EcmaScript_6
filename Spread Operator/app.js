042c26e27ce6f18603a068863da8493d43343d24
FarahSyed <farahsyed44@gmail.com>
2022-09-06 13:53:10 +0500

    Add Spread Operator

--- Spread Operator/app.js
+++ Spread Operator/app.js
@@ -64,10 +64,10 @@ const someMovies = [
 
 const copyInnermostArray = [...someMovies[3][2]];
 console.log(copyInnermostArray);
-document.write(`<h2>Extracted Movies:</h2>`);
+document.write(`<h2>Copied Movies:</h2>`);
 for (let i = 0; i < copyInnermostArray.length; i++) {
     document.write(`${copyInnermostArray[i]}<br />`);
 }
 // console.log(someMovies);
 // OR
-// document.write(`<h2>Extracted Movies:</h2><p>${copyInnermostArray}</p>`);
+// document.write(`<h2>Copied Movies:</h2><p>${copyInnermostArray}</p>`);
